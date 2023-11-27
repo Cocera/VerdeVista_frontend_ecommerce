1f5283ef84d69ef96aef5633f3cec4a825b26cca
Cocera <alcocera@gmail.com>
2023-11-27 13:36:10 +0100

    ProductProvider => App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import './fonts.scss';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ProductProvider } from './context/ProductContext/ProductState.jsx';
 import Home from './components/Home/Home.jsx';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
@@ -10,11 +11,13 @@ function App() {
   return (
     <>
     <BrowserRouter>
-      < Header />
-      < Routes>
-        < Route path="/" element={<Home />} />
-      </Routes>
-      < Footer />
+      <ProductProvider>
+        < Header />
+        < Routes>
+          < Route path="/" element={<Home />} />
+        </Routes>
+        < Footer />
+      </ProductProvider>
     </ BrowserRouter>
     </>
   )

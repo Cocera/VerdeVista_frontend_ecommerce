0eebfcbc9a9c3b065479d7a37be296f2f45f7787
Cocera <alcocera@gmail.com>
2023-11-28 13:07:29 +0100

    NavUserProfile menu dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           < Route path="/" element={<Home />} />
           < Route path="/login" element={<Login />} />
         </Routes>
-        < Footer />
+        {/* < Footer /> */}
       </ProductProvider>
     </ BrowserRouter>
     </>
--- src/components/Header/NavUserProfile/NavUserProfile.jsx
+++ src/components/Header/NavUserProfile/NavUserProfile.jsx
@@ -1,14 +1,27 @@
 // Conteneder que se rellene con la imagen del usuario que inicie sesion, si no hay sesion iniciada aparecera el icon
 // la 'ul' es el menu que aparecera al hacer hover sobre el icon de menu en desktop
 
+import { useState } from 'react';
+import './NavUserProfile.scss';
+
 const NavUserProfile = () => {
+
+    const [menuIsOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuIsOpen);
+    };
+
     return (
-        <>
-            <div className='material-symbols-outlined'>account_circle</div>
+        <>  
+        <div className='cont-nav-user'>
+            <button onClick={toggleMenu}>
+                <div className='material-symbols-outlined'>account_circle</div>
+            </button>
             <div className="hide">
                 <img src="" />
             </div>
-            <ul className="hide">
+            <ul className={`${menuIsOpen ? '' : 'hide'} nav-user-dropdown`}>
                 <li>
                     <span className="material-symbols-outlined">person</span>
                     <span>Profile</span>
@@ -22,6 +35,7 @@ const NavUserProfile = () => {
                     <span>Logout</span>
                 </li>
             </ul>
+        </div>
         </>
     );
 };

3fd0404d94bde16e7910b4e33dccd8ddbe01f03b
Cocera <alcocera@gmail.com>
2023-11-28 11:25:16 +0100

    NavUserProfile

--- src/components/Header/NavUserProfile/NavUserProfile.jsx
+++ src/components/Header/NavUserProfile/NavUserProfile.jsx
@@ -1,12 +1,27 @@
 // Conteneder que se rellene con la imagen del usuario que inicie sesion, si no hay sesion iniciada aparecera el icon
+// la 'ul' es el menu que aparecera al hacer hover sobre el icon de menu en desktop
 
 const NavUserProfile = () => {
     return (
         <>
-            <div className='material-symbols-outlined'>face</div>
+            <div className='material-symbols-outlined'>account_circle</div>
             <div className="hide">
                 <img src="" />
             </div>
+            <ul className="hide">
+                <li>
+                    <span className="material-symbols-outlined">person</span>
+                    <span>Profile</span>
+                </li>
+                <li>
+                    <span className="material-symbols-outlined">settings</span>
+                    <span>Settings</span>
+                </li>
+                <li>
+                    <span className="material-symbols-outlined">logout</span>
+                    <span>Logout</span>
+                </li>
+            </ul>
         </>
     );
 };
